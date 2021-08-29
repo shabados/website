224582bfaa5865fe797e7010e37763eabfc720d0
Bhajneet S.K <bhajneet@gmail.com>
2021-08-29 17:26:25 -0400

    feat: add MediaTiles to Viewer page

---
+++ src/components/MediaTile.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from 'react'
+import { createUseStyles } from 'react-jss'
+
+import { Breakpoints, widthLessThan } from '../theme'
+
+const useStyles = createUseStyles( {
+  tile: {
+    margin: '9rem 0',
+    display: 'flex',
+    borderRadius: '0.75rem',
+    gap: '1.5rem',
+    [ widthLessThan( Breakpoints.tablet ) ]: {
+      margin: '4.5rem 0',
+      flexWrap: 'wrap',
+    },
+    '& img': {
+      maxWidth: '600px',
+      borderRadius: '0.75rem',
+      [ widthLessThan( Breakpoints.tablet ) ]: {
+        order: -1,
+      },
+    },
+  },
+  text: {
+    alignSelf: 'center',
+  },
+} )
+
+type MediaTileProps = {
+  children: ReactNode,
+  order: number,
+  media: string,
+  alt: string,
+}
+
+const MediaTile = ( { children, ...props }: MediaTileProps ) => {
+  const classes = useStyles()
+  return (
+    <div className={classes.tile}>
+      <div className={classes.text} style={{ order: props.order }}>
+        {children}
+      </div>
+      <img src={props.media} alt={props.alt} />
+    </div>
+  )
+}
+
+export default MediaTile
