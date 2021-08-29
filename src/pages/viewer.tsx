b1f9eed395484dca21dcd955c62999e54fe5d121
Bhajneet S.K <bhajneet@gmail.com>
2021-08-29 12:59:44 -0400

    feat: add content to viewer

---
+++ src/pages/viewer.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useWindowWidth } from '@react-hook/window-size'
+import { createUseStyles } from 'react-jss'
+import { Parallax } from 'react-parallax'
+
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+import Section from '../components/Section'
+import { Breakpoints } from '../theme'
+import Link from '../components/Link'
+import Typography from '../components/Typography'
+import Navbar from '../components/Navbar'
+import Content from '../components/Content'
+
+const useStyles = createUseStyles( () => ( {
+  ib: {
+    display: 'inline-block',
+  },
+  heroMedia: {
+    borderRadius: 8,
+  },
+  hint: {
+    color: '#CCCCCC',
+  },
+} ) )
+
+const Viewer = () => {
+  const width = useWindowWidth()
+  const classes = useStyles()
+
+  return (
+    <>
+      <Navbar />
+      <Parallax
+        bgImage="/viewer-hero.svg"
+        bgImageStyle={{ maxWidth: `${width > Breakpoints.mobile ? '1000' : '500'}px`, top: `${width > Breakpoints.mobile ? '0.7' : '-20'}%` }}
+        strength={width > Breakpoints.mobile ? 300 : 200}
+        style={{ backgroundColor: '#dccbcb' }}
+      >
+        <Section
+          background="none"
+          color="white"
+          padding={`${width > Breakpoints.tablet ? '20rem' : '41vh'} 0px`}
+        >
+          <Content textAlign="center">
+            <Typography format="ginormous">Viewer</Typography>
+          </Content>
+        </Section>
+      </Parallax>
+      <Footer />
+    </>
+  )
+}
+
+export default Viewer
